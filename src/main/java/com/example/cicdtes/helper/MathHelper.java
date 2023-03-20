e9afe95199902c8423bd0c04cdffd7b46af5eb37
rhabichl <66315042+rhabichl@users.noreply.github.com>
2023-03-20 08:20:38 +0100

    changed workflow

--- src/main/java/com/example/cicdtes/helper/MathHelper.java
+++ src/main/java/com/example/cicdtes/helper/MathHelper.java
@@ -1,7 +1,6 @@
 package com.example.cicdtes.helper;
 
 public class MathHelper {
-
     // adds two numbers and returns the product
     public  static Integer add(int a, int b){
         return a + b;
