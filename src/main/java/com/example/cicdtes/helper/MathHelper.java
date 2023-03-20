355403d90683263c346c6a3e9ce95aa180349133
rhabichl <66315042+rhabichl@users.noreply.github.com>
2023-03-20 08:51:00 +0100

    added docker-container

--- src/main/java/com/example/cicdtes/helper/MathHelper.java
+++ src/main/java/com/example/cicdtes/helper/MathHelper.java
@@ -1,7 +1,7 @@
 package com.example.cicdtes.helper;
 
 public class MathHelper {
-    // adds two numbers and returns the product
+    // adds two numbers and returns the sum
     public  static Integer add(int a, int b){
         return a + b;
     }
